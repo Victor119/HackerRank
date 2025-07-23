6b25aad42a7f07c790232d3443bd80323376fb2c
Victor Rosca <roscavictor2268@gmail.com>
2025-07-23 15:17:35 +0300

    Add files via upload

---
+++ Arithmetic Operators/main.py
@@ -0,0 +1,17 @@
+def main():
+    a = input()
+    b = input()
+    
+    int_a = int(a)
+    int_b = int(b)
+    
+    sum = int_a + int_b
+    difference = int_a - int_b
+    product = int_a * int_b
+    
+    print(sum)
+    print(difference)
+    print(product)
+
+if __name__ == '__main__':
+    main()
